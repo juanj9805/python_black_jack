2511e2ae921ac2c18e0413de431d0fbea408399a
juan jose barrera <juanjoseb9805@gmail.com>
2025-11-16 23:45:48 -0500

    chore(game): The game feature already has all the features working good inside of it, But it's necessary to tweak the logic on the game

--- game.py
+++ game.py
@@ -1,53 +1,55 @@
 import random
 
-deck_cards = 10
-deck_cards_pint = ["🍀","❤️","♠️","💎"]
+CARDS = 10
+DECK_CARDS_SUITS = ["🍀","❤️","♠️","💎"]
 
 def create_deck ():
-    whole_deck = []
-    for card_pint in range(len(deck_cards_pint)):
-            for card in range(deck_cards):
-                  card_info = {
-                        "pint" : deck_cards_pint[card_pint],
-                        "number" : card + 1
-                  }
-                  whole_deck.append(card_info)
-    return whole_deck
+    deck = []
+    for suit in range(len(DECK_CARDS_SUITS)):
+        for card in range(CARDS):
+            card_info = {
+                "pint" : DECK_CARDS_SUITS[suit],
+                "number" : card + 1
+            }
+            deck.append(card_info)
+    return deck
 
-def shufle_cards (cards):
-    for card in range(len(cards)):      
-        random.shuffle(cards)
-    return cards
-      
-def remove_card (cards):
-    card_removed = cards.pop()
+def shufle_deck (deck):
+    random.shuffle(deck)
+    return deck
+ 
+def remove_card (deck):
+    card_removed = deck.pop()
     return card_removed
 
-     
 def game ():
-    player_total = 0
-    casino_total = 17
-    print("Welcome to 21 black jack")
-    player_money = 50
-    print("Let's start")
-    total = remove_card(shufle_cards(create_deck()))["number"] + remove_card(shufle_cards(create_deck()))["number"]
-    one_first_card = input(f"You already have {total}, Do you want another card: ")
-    if one_first_card == "yes":
-        total += remove_card(shufle_cards(create_deck()))["number"]
-        one_second_card = input(f"You already have {total}, Do you want another card: ")
-        if one_second_card == "yes":
-            total += remove_card(shufle_cards(create_deck()))["number"]
-            one_third_card = input(f"You already have {total}, Do you want another card: ")
-            if one_third_card == "yes":
-                total += remove_card(shufle_cards(create_deck()))["number"]
-                print(f"Your total score is {total}")
-            else:
-             print(f"your Score is {total}, Now is the dealer's turn, Good luck") 
-        else:
-            print(f"your Score is {total}, Now is the dealer's turn, Good luck") 
+    deck_shufled = shufle_deck(create_deck())
+    player_points = 0
+    casino_points = 0
+    winner = ""
+    player_points += remove_card(deck_shufled)["number"] + remove_card(deck_shufled)["number"]
+    while player_points <= 21:
+        choice = input(f"You already have {player_points}\nDo you want another card: ")
+        if choice == "yes":
+            player_points += remove_card(deck_shufled)["number"]
+        elif choice != "yes":
+            print(f"Your final score is {player_points}")
+            break
+    while casino_points <= 21 and casino_points < player_points :
+        casino_points += remove_card(deck_shufled)["number"]
+    print(f"Casino already has {casino_points}")
+    if player_points > casino_points and player_points <=21  :
+        winner += "Player"
+    elif player_points <= 21 and casino_points >21:
+        winner += "Player"
+    elif casino_points > player_points and casino_points <=21:
+        winner += "Casino"
+    elif casino_points > player_points and casino_points <=21:
+        winner += "Casino"
+    elif casino_points <= 21 and player_points >21:
+        winner += "Player"
     else:
-         print(f"your Score is {total}, Now is the dealer's turn, Good luck") 
+        winner += "Draw"
+    return print(f"The winner in this turn is\n{winner}")
 
-    return total
-         
 game()
